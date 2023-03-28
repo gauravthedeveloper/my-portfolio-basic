a1d6a790c39fe44c1839d6c314dda09107d57f2a
gauravthedeveloper <makecocgreatagain@gmail.com>
2023-03-29 00:57:24 +0530

    changes

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Twobuttons from "./Twobuttons";
-import ME from "../../Assets/ME2.png";
+import ME from "../../Assets/ME4.png";
 import HeaderSocials from "./HeaderSocials";
 import "./Header.css";
 
@@ -12,13 +12,14 @@ const Header = () => {
         <h1>Gourab Modak</h1>
         <h5 className="text-light">A Fierce Competitive Programmer</h5>
         <Twobuttons />
+        <HeaderSocials />
         <div className="me">
           <img src={ME} alt="me" />
         </div>
+
         <a href="#contact" className="scroll__down">
           Scroll Down
         </a>
-        <HeaderSocials />
       </div>
     </header>
   );
--- src/Components/header/Twobuttons.jsx
+++ src/Components/header/Twobuttons.jsx
@@ -3,12 +3,12 @@ import CV from "../../Assets/cv.pdf";
 import "./Header.css";
 const Twobuttons = () => {
   return (
-    <div className="twobuttons ">
+    <div className="twobuttons">
       <a href={CV} className="btn" download>
         Download CV
       </a>
-      <a href="#contact" className="btn-primary btn" download>
-        Lets talk
+      <a href="#contact" className="btn btn-primary">
+        Scroll Down
       </a>
     </div>
   );

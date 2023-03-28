a0b0b450ef7e3d00a2ff2a743e05e03e6fe71968
gauravthedeveloper <makecocgreatagain@gmail.com>
2023-03-28 14:06:26 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import "./App.css";
 import Header from "./Components/header/Header";
+import Nav from "./Components/nav/Nav";
+import Experiences from "./Components/experiences/Experiences";
+import About from "./Components/about/about";
+import Portfolio from "./Components/portfolio/portfolio";
+import Testimonials from "./Components/testimonials/Testimonials";
+import Contact from "./Components/contact/contact";
+import Footer from "./Components/footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Nav />
+      <About />
+      <Experiences />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
 }

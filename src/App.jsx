8325c69dcdf710dfe736100d91a160df387accf6
gauravthedeveloper <makecocgreatagain@gmail.com>
2023-03-28 14:38:37 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import "./App.css";
 import Header from "./Components/header/Header";
 import Nav from "./Components/nav/Nav";
 import Experiences from "./Components/experiences/Experiences";
-import About from "./Components/about/about";
-import Portfolio from "./Components/portfolio/portfolio";
+import About from "./Components/about/About";
+import Portfolio from "./Components/portfolio/Portfolio";
 import Testimonials from "./Components/testimonials/Testimonials";
-import Contact from "./Components/contact/contact";
+import Contact from "./Components/contact/Contact";
 import Footer from "./Components/footer/Footer";
 
 function App() {
